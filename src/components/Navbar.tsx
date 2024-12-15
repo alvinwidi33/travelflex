ec388630b5244e6e930e6ced87252daaa45ec65e
alvinwidi33 <alvin.widi@uiac.id>
2024-12-16 02:24:55 +0700

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "../images/logo.png";
+import logo from "../images/logo.svg";
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
@@ -8,34 +8,34 @@ function Navbar() {
   return (
     <nav className="w-full fixed top-0 z-50 shadow-md bg-white">
       <div className="container mx-auto flex items-center justify-between font-gilroy px-6 md:px-10 lg:px-14">
-        <img src={logo} className="w-16 h-auto" alt="Logo" />
+        <img src={logo} className="w-32 h-auto" alt="Logo" />
         <div className="flex space-x-4 flex-2 justify-center content-center items-center font-poppins">
           <Link
             to="/"
-            className={`text-sm md:text-base font-semibold px-6 py-5 ${
+            className={`text-sm md:text-base font-semibold px-6 py-7 ${
               isActive("/")
                 ? "bg-primary text-white"
-                : "text-black hover:bg-secondary px-6 py-6"
+                : "text-black hover:bg-secondary px-6 py-7"
             }`}
           >
             HOME
           </Link>
           <Link
             to="/about"
-            className={`text-sm md:text-base font-semibold px-6 py-5 ${
+            className={`text-sm md:text-base font-semibold px-6 py-7 ${
               isActive("/about")
                 ? "bg-primary text-white"
-                : "text-black hover:bg-secondary px-6 py-5"
+                : "text-black hover:bg-secondary px-6 py-7"
             }`}
           >
             ABOUT US
           </Link>
           <Link
             to="/reviews"
-            className={`text-sm md:text-base font-semibold px-6 py-5 ${
+            className={`text-sm md:text-base font-semibold px-6 py-7 ${
               isActive("/reviews")
                 ? "bg-primary text-white"
-                : "text-black hover:bg-secondary px-6 py-5"
+                : "text-black hover:bg-secondary px-6 py-7"
             }`}
           >
             REVIEWS
